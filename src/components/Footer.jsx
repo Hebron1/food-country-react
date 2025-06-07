1c4770961b56be6f47201038382f698a2364f287
Hebron <jsfast2@gmail.com>
2025-06-07 14:06:07 +0700

    update footer and notfoundpage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+    // const currentYear = new Date().getFullYear();
+    const currentYear = "March 2025";
   
     return (
       <footer className="bg-gray-800 text-white py-6 mt-auto">
         <div className="container mx-auto text-center">
           {/* Logo */}
-          <div className="mb-2">
-            <a href="#" className="text-3xl font-semibold">
-              <span>Logo</span>
+          <div className="mb-5">
+            <a href="https://github.com/Hebron1/food-country-react" className="text-5xl font-semibold flex justify-center">
+              <span><FaGithub className="hover:text-gray-300" /></span>
+              
             </a>
           </div>
   
-          {/* @nama developer dan tahun dibuat */}
           <p className="text-sm">
             &copy; {currentYear} @nama developer
           </p>
